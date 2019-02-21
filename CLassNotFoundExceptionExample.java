adeda4594f9e13fa7bd70630718f33e53a892b8c
japneetkaur21 <47344108+japneetkaur21@users.noreply.github.com>
2019-02-21 16:47:23 +0530

    Add files via upload

---
+++ CLassNotFoundExceptionExample.java
@@ -0,0 +1,12 @@
+public class CLassNotFoundExceptionExample {
+    public static void main(String[] args) {
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
