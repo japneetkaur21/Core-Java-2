528ee3e6ab7b2ec84e025137bd19c3744bf26788
japneetkaur21 <47344108+japneetkaur21@users.noreply.github.com>
2019-02-21 23:38:30 +0530

    Add files via upload

---
+++ Convert_code_using_nested_while_statements.java
@@ -0,0 +1,19 @@
+public class Convert_code_using_nested_while_statements {
+    public static void main(String[] args) {
+
+        int s = 0;
+        int t = 1;
+        int i=0;
+        while(i<10){
+            s=s+i;
+            int j=i;
+            while(j>0){
+                t=t*(j-i);
+                j--;
+            }
+            s=s*t;
+            System.out.println("T is " + t);
+            i++;
+        }
+        System.out.println("S is " + s);   }
+}
